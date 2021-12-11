0bd52f7ec121729f6f8f74d2a9fdca4f0949adb6
Julekha5 <mulani.julekha93@gmail.com>
2021-12-11 19:34:31 +0530

    first commit

---
+++ src/main/java/AddressBookMain.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+
+//main method
+public class AddressBookMain {
+
+	Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to AddressBook Management System");
+
+		
+	}
+}
